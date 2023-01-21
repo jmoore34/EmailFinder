f8453c17ee7cc35542b546bd837f2045058509a2
Jonathan Moore <jonm@duck.com>
2023-01-20 23:34:19 -0600

    visual fixes

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,15 @@
 browser.storage.local.get({ emails: ["no emails have been captured"] /* default */ }).then(storage => {
     const storedEmails = storage.emails
-    document.querySelector("textarea").innerText = storedEmails.join("\n")
+    document.querySelector("textarea").value = storedEmails.join("\r\n")
+    document.querySelector("#status > span").innerText = `Total ${storedEmails.length} stored emails    `
 }, err => console.log(err))
 
 
-document.querySelector("reset").addEventListener("click", async () => {
+document.getElementById("reset").addEventListener("click", async () => {
     console.log("reset button pressed")
         browser.storage.local.set({ emails: [] }).then(() => {
-            document.querySelector("textarea").innerText = ""
+            document.querySelector("textarea").value = ""
+            document.querySelector("#status").innerText = "Stored emails have been cleared"
         }).catch(err => {
             alert(`Error while resetting emails: ${err}`)
         })
